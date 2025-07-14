9ce6f372daff18a61c5dfaa95bc6039418d298e6
Tsilavina T <arvconsultance@gmail.com>
2025-07-14 10:07:38 +0300

    Add files via upload

---
+++ Login/Inscription.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Login/bootstrap-5.3.5-dist/css/bootstrap.min.css">
+    <script src="../Login/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
