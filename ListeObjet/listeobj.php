345847a14d8bc6997ff96e0c789310eaaa1e30ea
rwesley17 <mahelahwesley2@gmail.com>
2025-07-14 13:25:24 +0300

    Add files via upload

--- ListeObjet/listeobj.php
+++ ListeObjet/listeobj.php
@@ -59,20 +59,20 @@ echo '<img src="' . $imagePath . '" alt="Photo de profil" class="rounded-circle 
         </form>
 
         <?php
-        if (count($objets) > 0) {
-            echo '<table class="table table-bordered">';
-            echo '<thead><tr><th>Nom de l\'objet</th><th>Date de retour</th><th>Emprunt en cours</th></tr></thead><tbody>';
-            foreach ($objets as $row) {
-                echo '<tr>';
-                echo '<td>' . htmlspecialchars($row["nom_objet"]) . '</td>';
-                echo '<td>' . ($row["date_retour"] ? htmlspecialchars($row["date_retour"]) : 'N/A') . '</td>';
-                echo '<td>' . $row["emprunt_en_cours"] . '</td>';
-                echo '</tr>';
+            if (count($objets) > 0) {
+                echo '<table class="styled-table">';
+                echo '<thead><tr><th>Nom de l\'objet</th><th>Date de retour</th><th>Emprunt en cours</th></tr></thead><tbody>';
+                foreach ($objets as $row) {
+                    echo '<tr>';
+                    echo '<td>' . htmlspecialchars($row["nom_objet"]) . '</td>';
+                    echo '<td>' . ($row["date_retour"] ? htmlspecialchars($row["date_retour"]) : 'N/A') . '</td>';
+                    echo '<td>' . $row["emprunt_en_cours"] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</tbody></table>';
+            } else {
+                echo "<p>Aucun objet trouvé.</p>";
             }
-            echo '</tbody></table>';
-        } else {
-            echo "<p>Aucun objet trouvé.</p>";
-        }
         ?>
     </div>
 </body>
